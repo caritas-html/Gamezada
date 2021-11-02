19b245647938d20930f104f7b2001f7fc7ffa6ac
Davi Spindola <davispindola2828@gmail.com>
2021-11-01 22:13:03 -0300

    add minimal configs for rollup

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,7 +1,16 @@
-export default [{
-  input: './lib/index.js',
-    output: {
-      file: './build/lib-bundle.js',
-      format: 'es'
-    }
-  }]
+import RollupImage from '@rollup/plugin-image';
+import RollupTypescript from '@rollup/plugin-typescript'
+
+export default [
+  {
+    input: './lib/index.js',
+      output: {
+        file: './build/bundle.js',
+        format: 'es',
+      },
+    plugins: [
+      RollupImage(),
+      RollupTypescript({ lib: ["es5", "es6"], target: "es5" })
+    ]
+  }
+]
